7a48398667de512beb4b1402b38b2d9367006290
ziba <ziba>
2022-11-23 17:36:07 +0100

    promjena

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Container from "react-bootstrap/Container";
+import {Col, Row} from "react-bootstrap";
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>1 of 1</Col>
+      </Row>
+    </Container>
   );
 }
 
